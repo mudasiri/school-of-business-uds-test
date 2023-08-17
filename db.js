ae4294713d359a7aeb97fc12c68c515b42cad98d
Issah Mudasir <danladi496@gmail.com>
2023-08-17 00:31:30 +0000

    Switched-to-atlas-mongodb

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/udssob';
+//const dbUrl = 'mongodb://127.0.0.1:27017/udssob';
+const dbUrl = 'mongodb+srv://danladi496:CsDqahuwW4Lrkl6L@cluster0.genmgzy.mongodb.net';
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
